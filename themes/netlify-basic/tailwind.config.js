ccac2f74506c1182d146632234990224ef0f76e2
Peter Baker <pete@baker.co>
2022-09-25 22:39:27 -0400

    Updates

--- themes/netlify-basic/tailwind.config.js
+++ themes/netlify-basic/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Playfair Display', 'sans-serif'],
+      serif: ['Playfair Display', 'serif'],
+    },
   },
   plugins: [],
 };
